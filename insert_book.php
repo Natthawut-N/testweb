1106702565f1c818a3f7ed57e4cbabd953456168
natthawut-n <6306021412024@fitm.kmutnb.ac.th>
2021-03-11 15:54:15 +0700

    update

---
+++ insert_book.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <table>
+            <tr>
+                <td><label for="">book_name</label></td>
+                <td><input type="text" value=""></td>
+            </tr>
+            <tr>
+                <td><label for="">type_id</label></td>
+                <td><input type="text" value=""></td>
+            </tr>
+            <tr>
+                <td><label for="">status_id</label></td>
+                <td><input type="text" value=""></td>
+            </tr>
+            <tr>
+                <td><label for="">publish</label></td>
+                <td><input type="text" value=""></td>
+            </tr>
+            <tr>
+                <td><label for="">unit_price</label></td>
+                <td><input type="text" value=""></td>
+            </tr>
+            <tr>
+                <td><label for="">unit_rent</label></td>
+                <td><input type="text" value=""></td>
+            </tr>
+            <tr>
+                <td><label for="">day_amonunt</label></td>
+                <td><input type="text" value=""></td>
+            </tr>
+            <tr>
+                <td><label for="">picture</label></td>
+                <td><input type="text" value=""></td>
+            </tr>
+            <tr>
+                <td><label for="">book_date</label></td>
+                <td><input type="text" value=""></td>
+            </tr>
+            <td><input type="submit" value="submit"></td>
+        </table>
+    </form>
+</body>
+</html>
